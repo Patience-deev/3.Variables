4e16a11059f9134cabfc43e5a1f18cbe0fbae76a
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-09 11:07:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variable
+            String name = "John";
+            int age;
+
+            //inititialisation
+            name = "John";
+            age = 10;
+
+            //what will be seen on console
+            Console.WriteLine("There once was a boy called" +name);
+            Console.WriteLine(name + " was " + age + " of age. ");
+            Console.WriteLine(name + " really never liked his name ");
+
+            //change variable values
+            age = 18;
+            name = " Tom ";
+
+            Console.WriteLine(" At age " + age + " he changed his name to " + name);
+
+            //To freeze the console
+            Console.ReadLine();
+        }
+    }
+}
